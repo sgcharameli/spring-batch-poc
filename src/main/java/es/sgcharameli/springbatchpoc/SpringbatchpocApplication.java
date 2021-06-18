5d6d007f405fa5de9e84cf6037ba65896a0dbcea
Sergio <sgcharameli@gmail.com>
2021-06-19 00:26:11 +0200

    Initial commit

---
+++ src/main/java/es/sgcharameli/springbatchpoc/SpringbatchpocApplication.java
@@ -0,0 +1,13 @@
+package es.sgcharameli.springbatchpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchpocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/sgcharameli/springbatchpoc/SpringbatchpocApplicationTests.java
@@ -0,0 +1,13 @@
+package es.sgcharameli.springbatchpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchpocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
